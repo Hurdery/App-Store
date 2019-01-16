f1c2ae4b572916e5e184b84f6b9fdd8ea3a2e579
GuaZHA <1589353390@qq.com>
2019-01-16 17:36:01 +0800

    Initial Commit

---
+++ appstore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  appstore
+//
+//  Created by guazhac on 2019/1/16.
+//  Copyright © 2019 st. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
